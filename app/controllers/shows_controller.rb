33267239cd703f016ef4efb641f39e9d2fb2a943
Sam Park and Stephanie Gurung <sam+stephanie@>
2016-09-21 11:50:36 -0700

    allow the user to add a new show

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -4,4 +4,20 @@ class ShowsController < ApplicationController
     @show = Show.new
   end
 
+  def create
+    @show = Show.new(show_params)
+    if @show.save
+      flash[:notice] = "Show successfully added!"
+      respond_to do |format|
+        format.html { redirect_to :back }
+        format.js
+      end
+    else
+      render :new
+    end
+  end
+private
+  def show_params
+    params.require(:show).permit(:name, :description, :media_type, :category)
+  end
 end
